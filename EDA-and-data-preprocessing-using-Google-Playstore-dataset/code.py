29e58f19969d5363b79d03a92c6d7ff53ab4e095
Martand Dev <devtruerimco@gmail.com>
2020-01-15 03:09:03 +0530

    Update code.py

--- EDA-and-data-preprocessing-using-Google-Playstore-dataset/code.py
+++ EDA-and-data-preprocessing-using-Google-Playstore-dataset/code.py
@@ -4,84 +4,128 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 #Code starts here
+
+#Loading the data
 data=pd.read_csv(path)
-data.hist(column="Rating")
 
+#Plotting histogram of Rating
+data['Rating'].plot(kind='hist')
 
-data=data[data["Rating"]<=5]
-data.hist(column="Rating")
+plt.show()
 
+#Subsetting the dataframe based on `Rating` column
+data=data[data['Rating']<=5]
+
+#Plotting histogram of Rating
+data['Rating'].plot(kind='hist')   
 
 #Code ends here
 
 
 # --------------
-# code starts here
-total_null=data.isnull().sum()
-#total_null=total_null.values.tolist()
+#Code starts here
 
-percent_null=total_null/data.isnull().count()
+#Sum of null values of each column
+total_null = data.isnull().sum()
 
-missing_data=pd.concat([total_null,percent_null],axis=1,keys=["Total","Percent"])
-print("missing_data:",missing_data)
+#Percentage of null values of each column
+percent_null = (total_null/data.isnull().count())
 
-data.dropna(inplace=True)
-total_null_1=data.isnull().sum()
-percent_null_1=total_null_1/data.isnull().count()
+#Concatenating total_null and percent_null values
+missing_data = pd.concat([total_null, percent_null], axis=1, keys=['Total', 'Percent'])
 
-missing_data_1=pd.concat([total_null_1,percent_null_1],axis=1,keys=["Total","Percent"])
-print("missing_data_1:",missing_data_1)
+print(missing_data)
 
+#Dropping the null values
+data.dropna(inplace = True)
 
+#Sum of null values of each column
+total_null_1 = data.isnull().sum()
 
-# code ends here
+#Percentage of null values of each column
+percent_null_1 = (total_null_1/data.isnull().count())
+
+#Concatenating total_null and percent_null values
+missing_data_1 = pd.concat([total_null_1, percent_null_1], axis=1, keys=['Total', 'Percent'])
+
+print(missing_data_1)
+
+#Code ends here
 
 
 # --------------
+
+#Category vs Rating
 
 #Code starts here
 
-g=sns.catplot(x="Category",y="Rating",kind="box",height=10,data=data)
-g.set_xticklabels(rotation=90)
-g.set_titles("Rating vs Category [BoxPlot]")
-#Code ends here
+#Setting the figure size
+plt.figure(figsize=(10,10))
 
+#Plotting boxplot between Rating and Category
+cat= sns.catplot(x="Category",y="Rating",data=data, kind="box", height = 10)
+
+#Rotating the xlabel rotation
+cat.set_xticklabels(rotation=90)
+
+#Setting the title of the plot
+plt.title('Rating vs Category [BoxPlot]',size = 20)
+
+
+#Code ends here
 
 # --------------
 #Importing header files
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
 #Code starts here
-print("value counts of Installs column(before rmoving +,):",data["Installs"].value_counts())
 
-data['Installs'] = data['Installs'].str.replace("[+,]","").astype(int)
-print("value counts of Installs column(after rmoving +,):",data["Installs"].value_counts())
+#Removing `,` from the column
+data['Installs']=data['Installs'].str.replace(',','')
 
-#LabelEncoding
+#Removing `+` from the column
+data['Installs']=data['Installs'].str.replace('+','')
+
+#Converting the column to `int` datatype
+data['Installs'] = data['Installs'].astype(int)
+
+#Creating a label encoder object
 le=LabelEncoder()
-data["Installs"]=le.fit_transform(data["Installs"])
 
-#Plotting
-sns.regplot(x="Installs",y="Rating",data=data).set_title("Rating vs Installs [RegPlot]")
+#Label encoding the column to reduce the effect of a large range of values
+data['Installs']=le.fit_transform(data['Installs'])
 
+#Setting figure size
+plt.figure(figsize = (10,10))
 
+#Plotting Regression plot between Rating and Installs
+sns.regplot(x="Installs", y="Rating", color = 'teal',data=data)
 
+#Setting the title of the plot
+plt.title('Rating vs Installs[RegPlot]',size = 20)
 
 #Code ends here
 
 
-
 # --------------
 #Code starts here
-print("Value counts of price:\n",data["Price"].value_counts())
 
-data["Price"]=data["Price"].str.replace("$","").astype(float)
+#Removing the dollar sign from the column
+data['Price'] = data['Price'].str.replace('$','')
 
-sns.regplot(x="Price",y="Rating",data=data).set_title("Rating Vs Price [RegPlot]")
+#Converting the column to float
+data['Price'] = data['Price'].astype(float)
 
+#Setting the figure size
+plt.figure(figsize = (10,10))
 
+#Plotting Regression plot between Rating and Price
+sns.regplot(x="Price", y="Rating", color = 'darkorange',data=data)
 
+#Setting the plot title
+plt.title('Rating vs Price[Reg Plot]',size = 20)
 
 #Code ends here
 
@@ -105,27 +149,47 @@ gr_mean=gr_mean.sort_values(by=["Rating"])
 print("Lowest Average Rating(Genrewise):",gr_mean.iloc[0:1,])
 print("Highest Average Rating(Genrewise):",gr_mean.iloc[-1:,])
 
-
 #Code ends here
 
 
-# --------------
-
+# ------
 #Code starts here
-#print("Last Updated",data["Last Updated"])
-data["Last Updated"]=pd.to_datetime(data["Last Updated"])
-#print("data[Last Updated]:",data["Last Updated"])
-max_date=data["Last Updated"].max()
 
-data["Last Updated Days"]=(max_date-data["Last Updated"]).dt.days
+#Converting the column into datetime format
+data['Last Updated'] = pd.to_datetime(data['Last Updated'])
 
-sns.regplot(x="Last Updated Days",y="Rating",data=data).set_title("Rating vs Last Updated [RegPlot]")
+#Creating new column having `Last Updated` in days
+data['Last Updated Days'] = (data['Last Updated'].max()-data['Last Updated'] ).dt.days 
+
+#Setting the size of the figure
+plt.figure(figsize = (10,10))
+
+#Plotting a regression plot between `Rating` and `Last Updated Days`
+sns.regplot(x="Last Updated Days", y="Rating", color = 'lightpink',data=data )
+
+#Setting the title of the plot
+plt.title('Rating vs Last Updated [RegPlot]',size = 20)
+
+#Code ends here--------
 
 
+#Code starts here-------
 
+#Converting the column into datetime format
+data['Last Updated'] = pd.to_datetime(data['Last Updated'])
 
+#Creating new column having `Last Updated` in days
+data['Last Updated Days'] = (data['Last Updated'].max()-data['Last Updated'] ).dt.days 
 
+#Setting the size of the figure
+plt.figure(figsize = (10,10))
 
-#Code ends here
+#Plotting a regression plot between `Rating` and `Last Updated Days`
+sns.regplot(x="Last Updated Days", y="Rating", color = 'lightpink',data=data )
+
+#Setting the title of the plot
+plt.title('Rating vs Last Updated [RegPlot]',size = 20)
+
+#Code ends here------
 
 
